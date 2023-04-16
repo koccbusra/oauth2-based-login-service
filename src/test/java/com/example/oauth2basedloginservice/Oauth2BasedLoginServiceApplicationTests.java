ec965c36accc8d597ae36f793f6e9507da2341ba
Busra Koc <koccbusra@gmail.com>
2023-04-16 15:25:41 +0200

    github and google aouth2 services are added.

---
+++ src/test/java/com/example/oauth2basedloginservice/Oauth2BasedLoginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauth2basedloginservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2BasedLoginServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
